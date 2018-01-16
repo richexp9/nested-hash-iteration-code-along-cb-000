2c6cf6749ebd6d7c34c3d020a5a6a483cc5b689b
Richard Loera <richexp9@gmx.com>
2018-01-16 20:02:22 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,9 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  contacts.each { |person, contact_details_hash|  }
+  contacts.each { |person, contact_details_hash|
+    binding.pry
+    }
 
 
   # contacts.each { |names, data|
